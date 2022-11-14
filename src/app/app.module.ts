eabf6b7281b0676ff6fdc144ce98b34b4dff913d
onseas <osmarneto750@gmail.com>
2022-11-14 18:33:27 -0300

    Removendo sidebar, adicionando navbar e home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatSidenavModule,
+    MatMenuModule,
     MatListModule
   ],
   providers: [],
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutes } from './home.routing';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutes,
+
+  ]
+})
+export class HomeModule { }
